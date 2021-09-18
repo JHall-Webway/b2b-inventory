cbd859797a8d74be33183bb0aefe0a6b477f4264
Deku <zachdsamson@gmail.com>
2021-09-18 16:50:23 -0700

    delete and bulk add products

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -27,5 +27,35 @@ router.post('/', (req, res) => {
     })
 });
 
+// DELETE a product and any order detail containing that product
+router.delete('/:id', (req,res) => {
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No product found with this id'});
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// CREATE many records from an excel doc
+router.post('/excel', (req, res) => {
+    const productArray = req.body;
+
+    Product.bulkCreate(productArray)
+    .then(dbProductData => {
+        res.json('UPLOADED')
+    })
+})
+
 
 module.exports = router;
