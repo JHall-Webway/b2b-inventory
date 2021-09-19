ec8798aa52d40e7731456f8bf8ba8dc127b9a8d7
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-19 13:29:50 -0700

    Create login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,4 +22,14 @@ router.get('/', (req, res) => {
         });
 });
 
+// if user is logged in redirect to users account else redirect to login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
 module.exports = router;
