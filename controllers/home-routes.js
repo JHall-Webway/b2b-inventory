d944631d353779feff7057462cedc00966a13815
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-19 14:11:18 -0700

    Set up signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,4 +32,14 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// if user session exists redirect to homepage else render signup
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
 module.exports = router;
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -2,22 +2,22 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && password) {
+    if (username && email && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
+                email,
                 password
             }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: { 'Content-Type': 'application/json' }
         });
-
+        // check the response status
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
