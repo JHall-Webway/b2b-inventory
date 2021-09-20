f1b5c487b7c09b7ebdabd10086d7d0dcdeea8c9f
Deku <zachdsamson@gmail.com>
2021-09-20 12:16:54 -0700

    small changes

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,24 +1,5 @@
-const { use } = require('../controllers/api/user-routes');
 const { User } = require('../models');
 
-// const userData = [{
-//         username: 'David',
-//         email: 'David@gmail.com',
-//         password: '1234'
-//     },
-//     {
-//         username: 'James',
-//         email: 'James@gmail.com',
-//         password: '1234'
-//     },
-//     {
-//         username: 'Zach',
-//         email: 'Zach@gmail.com',
-//         password: '1234'
-//     }
-// ];
-
-// const seedUsers = () => User.bulkCreate(userData);
 
 const user1 = {
     username: 'David',
