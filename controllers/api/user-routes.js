b90394a31bd8c29c921a12a932d61e3b24b043b6
Deku <zachdsamson@gmail.com>
2021-09-20 12:37:50 -0700

    add logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,4 +66,15 @@ router.post('/login', (req, res) => {
         });
 });
 
+// Logout route
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
