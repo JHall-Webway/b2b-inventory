af1898ffb7a91b8189017ed93843cc28b4c46c45
Deku <zachdsamson@gmail.com>
2021-09-21 18:23:07 -0700

    order detail form generates--- need to post to order detail

--- public/javascript/add-order-detail.js
+++ public/javascript/add-order-detail.js
@@ -19,8 +19,19 @@ function createline(prodObj) {
     var formGroup = document.createElement('div');
     formGroup.classList.add('form-group');
 
+    var labelEl = document.createElement('label');
+    labelEl.setAttribute('for', 'select-prod');
+    labelEl.innerHTML = "Select product";
+
     var lineEl = document.createElement('select');
     lineEl.classList.add('prod-select');
+    lineEl.setAttribute('id', 'select-prod');
+
+    var inputQtyEl = document.createElement('input');
+    inputQtyEl.setAttribute('type', 'number');
+    inputQtyEl.setAttribute('min', '1');
+    inputQtyEl.setAttribute('max', '1000');
+    inputQtyEl.classList.add('qty-input');
 
     prodObj.forEach(prod => {
         var dropDownEl = document.createElement('option');
@@ -28,8 +39,11 @@ function createline(prodObj) {
         dropDownEl.text = `${prod.product_name}`
         lineEl.appendChild(dropDownEl);
     })
-    
-    lineForm.appendChild(lineEl);
+
+    formGroup.appendChild(labelEl);
+    formGroup.appendChild(lineEl);
+    formGroup.appendChild(inputQtyEl);
+    lineForm.appendChild(formGroup);
 }
 
 
