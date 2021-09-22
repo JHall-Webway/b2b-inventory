1a1592eab05d550965edb59a05b830070081efb7
Deku <zachdsamson@gmail.com>
2021-09-22 13:30:21 -0700

    report semi working

--- public/javascript/generate-report.js
+++ public/javascript/generate-report.js
@@ -1,5 +1,4 @@
-
-
+const reportHolder = document.getElementById('rep-table');
 
 async function grabProducts() {
     const invQtyArray = [];
@@ -33,10 +32,44 @@ async function grabProducts() {
         console.log(invQtyArray);
         //holds the TOTAL orders on a product (matching index)
         console.log(orderArray);
+
+        generateReport(invNameArray, invQtyArray, orderArray);
     } else {
         alert(response.statusText);
 
     }
+}
+
+function generateReport(prodNames, stock, orders) {
+    reportHolder.innerHTML = "";
+    let tableEl = document.createElement('table');
+    tableEl.classList.add('table');
+
+    let theadEl = document.createElement('thead');
+
+    let thProd = document.createElement('th');
+    thProd.innerHTML = "Product";
+
+    let thStock = document.createElement('th');
+    thStock.innerHTML = "Current Stock";
+
+    let thDemand = document.createElement('th');
+    thDemand.innerHTML = "Total Demand";
+
+    let thDelta = document.createElement('th');
+    thDelta.innerHTML = "Delta";
+
+    // add headers to table header
+    theadEl.appendChild(thProd);
+    theadEl.appendChild(thStock);
+    theadEl.appendChild(thDemand);
+    theadEl.appendChild(thDelta);
+    // add table header to table
+    tableEl.appendChild(theadEl);
+
+
+    // add table to DOM
+    reportHolder.appendChild(tableEl);
 }
 
 
